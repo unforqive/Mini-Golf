fa23b34cbcfcbfbe2aa781930445c96f38f455c2
unforqive <56460149+joshuawilloughby@users.noreply.github.com>
2021-02-20 17:59:06 +0000

    1.0.5

--- Assets/Scripts/Game/CameraController.cs
+++ Assets/Scripts/Game/CameraController.cs
@@ -4,29 +4,72 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float speed = 5;
+    public Transform cameraParent;
+    public float speed = 5f;
+    public float moveSpeed = 0.25f;
 
-    public Transform target;
+    public Transform player;
+
+    private Quaternion camRotation;
+    private float lookUpMin = -50;
+    private float lookUpMax = 50;
+
+    public float newMinX;
+    public float newMaxX;
+
+    public float newMinY;
+    public float newMaxY;
+
+    public float newMinZ;
+    public float newMaxZ;
+
+    public float cameraSmoothFactor = 1;
 
     void Update()
     {
-        if(Input.GetMouseButton(1))
+        if (Input.GetMouseButton(1))
         {
-            transform.eulerAngles += speed * new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0);
+            camRotation.x += speed * Input.GetAxis("Mouse Y") * cameraSmoothFactor * (-1);
+            camRotation.y += speed * Input.GetAxis("Mouse X") * cameraSmoothFactor;
+
+            camRotation.x = Mathf.Clamp(camRotation.x, lookUpMin, lookUpMax);
+
+            cameraParent.localRotation = Quaternion.Euler(camRotation.x, camRotation.y, 0);
         }
 
-        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        Vector3 p_Velocity = new Vector3();
+        if (Input.GetKey(KeyCode.W))
         {
-            float scroll = Input.GetAxis("Mouse ScrollWheel");
-            transform.Translate(0, 0, scroll * speed, Space.Self);
+            p_Velocity += new Vector3(0, 0, 1);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            p_Velocity += new Vector3(0, 0, -1);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            p_Velocity += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            p_Velocity += new Vector3(1, 0, 0);
         }
 
-        if (Input.GetAxis("Mouse ScrollWheel") > 0)
-        {
-            float scroll = Input.GetAxis("Mouse ScrollWheel");
-            transform.Translate(0, 0, scroll * speed, Space.Self);
-        }
+        cameraParent.Translate(p_Velocity * moveSpeed, Space.Self);
 
-        //transform.position = new Vector3(Mathf.Clamp(target.position.x, 1f, 1f), Mathf.Clamp(target.position.y, 1f, 1f), Mathf.Clamp(target.position.y, 1f, 1f));
+        float minX = player.position.x - newMinX;
+        float maxX = player.position.x - newMaxX;
+
+        float minY = player.position.y - newMinY;
+        float maxY = player.position.y - newMaxY;
+
+        float minZ = player.position.z - newMinZ;
+        float maxZ = player.position.z - newMaxZ;
+
+        float posX = Mathf.Clamp(transform.position.x, minX, maxX);
+        float posY = Mathf.Clamp(transform.position.y, minY, maxY);
+        float posZ = Mathf.Clamp(transform.position.z, minZ, maxZ);
+
+        cameraParent.position = new Vector3(posX, posY, posZ);
     }
 }
